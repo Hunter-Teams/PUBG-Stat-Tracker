7c96ac99bc26d4e5d81b8d15c9d9488472e56cba
Cordell Hurst <16294570+theartfuldodger1@users.noreply.github.com>
2019-01-21 23:57:40 -0500

    index.html title and favicon.ico changed

--- src/components/advertising.js
+++ src/components/advertising.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "../index.css";
 
+//simple func that return image of an ad
 class Advertising extends Component {
   render() {
-    //var { rank, name, kd, wr, kills, wins, matches, score } = this.props.data;
-
     return (
       <div>
+        <div className="flex-container"></div>
         <a href="https://www.cyberpowerpc.com/page/Intel/Esports/?utm_source=asc&utm_medium=display&utm_campaign=asc-display-retargeting&gclid=EAIaIQobChMIgqP90tX_3wIVEMfACh1imA1ZEAEYASAAEgKoGPD_BwE">
           <div className="advert" />
         </a>
