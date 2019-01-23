68c39ba4e088d485d9d6afd094c77162f5b3cd76
shanisaar <shanisaar@gmail.com>
2019-01-23 10:21:37 -0500

    removing build from gitignore 2

---
+++ build/precache-manifest.883d4a10e30cce0625694526b21641b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5060035dbfc892e8eb7a409163454134",
+    "url": "/static/media/madmax.5060035d.jpg"
+  },
+  {
+    "revision": "ba4736eb8fcbc74ac639a7f7e16a28f4",
+    "url": "/static/media/gearCut.ba4736eb.png"
+  },
+  {
+    "revision": "2b13d3ae5e61d28f6d0b6c378469c8b1",
+    "url": "/static/media/cuny2x.2b13d3ae.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0f2e7cc95e51981b876",
+    "url": "/static/js/main.e0f2e7cc.chunk.js"
+  },
+  {
+    "revision": "d8c1c108a92f92c3864c",
+    "url": "/static/js/1.d8c1c108.chunk.js"
+  },
+  {
+    "revision": "e0f2e7cc95e51981b876",
+    "url": "/static/css/main.81f06eb3.chunk.css"
+  },
+  {
+    "revision": "d8c1c108a92f92c3864c",
+    "url": "/static/css/1.acf6a240.chunk.css"
+  },
+  {
+    "revision": "001c679fc4a58ea36a06eb76046ad971",
+    "url": "/index.html"
+  }
+];
