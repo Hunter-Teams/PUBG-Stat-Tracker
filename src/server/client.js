d6df528b69b7b5ad0ab2e55cc842166d821424db
shanisaar <shanisaar@gmail.com>
2019-01-23 15:16:32 -0500

    configured pg to connect to heroku

--- src/server/client.js
+++ src/server/client.js
@@ -22,18 +22,17 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
-  sslmode=require
+  ssl: true
 });
 
 client.connect();
 
 client.query('SELECT * from users', (err, res) => {
   if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+  // for (let row of res.rows) {
+  //   console.log(JSON.stringify(row));
+  // }
+  // client.end();
 });
 
 // export the client to use elsewhere in your express app for working with queries.
