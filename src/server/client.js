474a7eb153d6553cd4ef58dcfc2e257d5d7cc8ab
shanisaar <shanisaar@gmail.com>
2019-01-23 15:05:32 -0500

    configured pg to connect to heroku

--- src/server/client.js
+++ src/server/client.js
@@ -1,25 +1,24 @@
-// configuring PostgreSQL locally
-const pg = require("pg");
+// // configuring PostgreSQL locally
+// const pg = require("pg");
+//
+// // create a new client
+// const client = new pg.Client({
+//     user: "postgres",
+//     host: 'localhost',
+//     database: 'pubg_stats',
+//     password: '123456',  //  ENTER YOUR PASSWORD!!!
+//     port: 5432
+// })
+// // connect your client to your database
+// client.connect();
+//
+// client.query('select * from users', (err, data) => {
+//     if (err) console.error(err);
+// })
 
-// create a new client
-const client = new pg.Client({
-    user: "postgres",
-    host: 'localhost',
-    database: 'pubg_stats',
-    password: '123456',  //  ENTER YOUR PASSWORD!!!
-    port: 5432
-})
-// connect your client to your database
-client.connect();
-
-client.query('select * from users', (err, data) => {
-    if (err) console.error(err);
-})
-// export the client to use elsewhere in your express app for working with queries.
-module.exports = client;
 
 // connecting to Heroku
-/* const { Client } = require('pg');
+const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -29,11 +28,13 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+client.query('SELECT * from users', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
   client.end();
 });
-*/
+
+// export the client to use elsewhere in your express app for working with queries.
+module.exports = client;
